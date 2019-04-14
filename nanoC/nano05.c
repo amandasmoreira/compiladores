d097b0b6299d470a3ce6696bc6d233fa3e1e12fe
amandasmoreira <48495076+amandasmoreira@users.noreply.github.com>
2019-04-14 11:00:07 -0300

    Add files via upload

---
+++ nanoC/nano05.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  n = 2;
+  printf("%d\n", n);
+  return 0;
+}
---
+++ nanoC/nano09.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  n = 1 + 1 / 2;
+  if (n == 1)
+    printf("%d\n", n);
+  else
+    printf("%d\n", 0);
+  return 0;
+}
